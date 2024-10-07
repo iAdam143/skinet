23980b9b5074974b51ef64f46a8a4609f8482a3c
Adnan Khalid <dkhalid46@gmail.com>
2024-10-07 16:16:20 +0200

    Added Repository Pattern and Also added teh seed data

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +10,23 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("Default Connection"));
 });
+
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+
 var app = builder.Build();
 
 app.MapControllers();
-
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
 app.Run();
